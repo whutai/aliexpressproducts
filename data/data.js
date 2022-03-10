ccf5f0fd5c7ef1a7e707121d76e66996ed80d15f
Whutai <taiphantmt@gmail.com>
2022-03-10 09:02:43 +0700

    update img qualify

--- data/data.js
+++ data/data.js
@@ -3,68 +3,68 @@ export const productList = [
     name: "Makeup Organizer",
     afflink: "https://s.click.aliexpress.com/e/_AcRubn",
     imglink:
-      "//ae01.alicdn.com/kf/Se1fc8c8c21954c41840cda69fcee37c0R.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/Se1fc8c8c21954c41840cda69fcee37c0R.jpg_350x350.jpg"
   },
   {
     name: "Pinkiou Nail",
     afflink: "https://s.click.aliexpress.com/e/_AmKnZX",
     imglink:
-      "//ae01.alicdn.com/kf/H090deaa4b7684415bb7b536d058514aai.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/H090deaa4b7684415bb7b536d058514aai.jpg_350x350.jpg"
   },
   {
     name: "Nail Stickers",
     afflink: "https://s.click.aliexpress.com/e/_AWodUd",
     imglink:
-      "//ae01.alicdn.com/kf/H7e8ac10e5ab2453796cdebe576b601f4R.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/H7e8ac10e5ab2453796cdebe576b601f4R.jpg_350x350.jpg"
   },
   {
     name: "Food Save",
     afflink: "https://s.click.aliexpress.com/e/_AnbyLT",
     imglink:
-      "//ae01.alicdn.com/kf/Ha93d6cf8f30647348d32124fa585967eg.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/Ha93d6cf8f30647348d32124fa585967eg.jpg_350x350.jpg"
   },
   {
     name: "Mini Heat Sealer ",
     afflink: "https://s.click.aliexpress.com/e/_AEu5g9",
     imglink:
-      "//ae01.alicdn.com/kf/H0f1653dbf69b492e8ce32e6140de512fh.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/H0f1653dbf69b492e8ce32e6140de512fh.jpg_350x350.jpg"
   },
   {
     name: "Hooks Wall ",
     afflink: "https://s.click.aliexpress.com/e/_AaNB4Z",
     imglink:
-      "//ae01.alicdn.com/kf/Sc1ba729c6cf542a5b023fb8acaaf0918u.png_140x140.png"
+      "//ae01.alicdn.com/kf/Sc1ba729c6cf542a5b023fb8acaaf0918u.png_350x350.png"
   },
 
   {
     name: "Shoe Rack",
     afflink: "https://s.click.aliexpress.com/e/_9y2gyD",
     imglink:
-      "//ae01.alicdn.com/kf/Hdff9b31211c34ad8862ee5c50b6beb05n.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/Hdff9b31211c34ad8862ee5c50b6beb05n.jpg_350x350.jpg"
   },
   {
     name: "Electric Air",
     afflink: "https://s.click.aliexpress.com/e/_9wEYyv",
     imglink:
-      "//ae01.alicdn.com/kf/Hdfe1ee4348ce40b383b28de843bf7e517.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/Hdfe1ee4348ce40b383b28de843bf7e517.jpg_350x350.jpg"
   },
   {
     name: "Storage Basket",
     afflink: "https://s.click.aliexpress.com/e/_A4ChyH",
     imglink:
-      "//ae01.alicdn.com/kf/Hb36ea313ee614d3087a65fc4720eed22U.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/Hb36ea313ee614d3087a65fc4720eed22U.jpg_350x350.jpg"
   },
   {
     name: "Portable Large",
     afflink: "https://s.click.aliexpress.com/e/_AMdjeD",
     imglink:
-      "//ae01.alicdn.com/kf/H8d0de0da106844d3b54182b33166b58ex.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/H8d0de0da106844d3b54182b33166b58ex.jpg_350x350.jpg"
   },
   {
     name: "Laptop Sleeve Case",
     afflink: "https://s.click.aliexpress.com/e/_AKiSw9",
     imglink:
-      "//ae01.alicdn.com/kf/H4eececfb299f47698a6b63e4dbe511ffq.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/H4eececfb299f47698a6b63e4dbe511ffq.jpg_350x350.jpg"
   }
 ];
 export const toolList = [
@@ -72,18 +72,18 @@ export const toolList = [
     name: "Hinge Repair",
     afflink: "https://s.click.aliexpress.com/e/_AFYfgl",
     imglink:
-      "//ae01.alicdn.com/kf/S5e2794da3cbd431ca65d72141363b64b9.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/S5e2794da3cbd431ca65d72141363b64b9.jpg_350x350.jpg"
   },
   {
     name: "Moving Artifact Household",
     afflink: "https://s.click.aliexpress.com/e/_9Qw0BB",
     imglink:
-      "//ae01.alicdn.com/kf/Hba04ab4146264c39aa4d41932e5b8c9bQ.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/Hba04ab4146264c39aa4d41932e5b8c9bQ.jpg_350x350.jpg"
   },
   {
     name: "Mini Heat Sealer ",
     afflink: "https://s.click.aliexpress.com/e/_AEu5g9",
     imglink:
-      "//ae01.alicdn.com/kf/H0f1653dbf69b492e8ce32e6140de512fh.jpg_140x140.jpg"
+      "//ae01.alicdn.com/kf/H0f1653dbf69b492e8ce32e6140de512fh.jpg_350x350.jpg"
   }
 ];
