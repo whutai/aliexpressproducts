d764b7c34b776e10cbf3aa532a506f77b7c65134
Whutai <taiphantmt@gmail.com>
2022-03-17 09:03:57 +0700

    update

--- pages/tools.js
+++ pages/tools.js
@@ -114,6 +114,21 @@ export default function Home() {
               loader={<div class="dashed-loading"></div>}
               className="product-list"
             >
+              <div className="product-item">
+                <a
+                  href="https://s.click.aliexpress.com/e/_AraOpt"
+                  target="_blank"
+                  className="afflink"
+                >
+                  <img
+                    src="//ae01.alicdn.com/kf/H39355416bbff46329b40a320ac28ee74X.jpg_350x350.jpg"
+                    className="imglink"
+                  />
+                </a>
+                <div className="product-text">
+                  <p>Cheap Woodworking Tools, High Quality </p>
+                </div>
+              </div>
               {listProduct.map((item, index) => (
                 <div className="product-item" key={index}>
                   <a href={item.afflink} target="_blank" className="afflink">
